e9d03ab93fc56e2d6b6377d0d555a2071ef26c56
Paul <paulmulin1@gmail.com>
2023-03-14 10:50:59 +0100

    resolve main conflicts

--- main.cpp
+++ main.cpp
@@ -1,22 +1,13 @@
-
 #include "main.hpp"
 
 int main(int ac, char **av)
 {
-	// std::string name1 = "channel1";
-	// std::string name2 = "channel2";
-	// std::string name3 = "channel3";
-	// Channel test;
-	// test.addExistingChannels(name1);
-	// test.addExistingChannels(name2);
-	// test.addExistingChannels(name3);
-	// test.printExistingChannels();
-	// handlerUserInput();
 	if (ac != 3)
 	{
 		std::cerr << "./ircserv [port number] [password]\n";
 		return (-1);
 	}
-	launchServer(av[1], av[2]);
+
+	Server ircserv(av[1], av[2]);
 	return (0);
 }
--- main.hpp
+++ main.hpp
@@ -2,4 +2,3 @@
 
 #include "srcs/server/Server.hpp"
 #include "srcs/channel/Channel.hpp"
-#include "srcs/channel/Channel.cpp"
