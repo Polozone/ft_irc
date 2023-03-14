80981ce6965a788b50be3951bf559e03340460dd
Theo Deville <tdeville@z1r3p2.42lyon.fr>
2023-03-14 10:56:47 +0100

    Class client

--- srcs/client/Client.hpp
+++ srcs/client/Client.hpp
@@ -1,3 +1,21 @@
+#pragma once
 
+#include <string>
 
+class Client
+{
 
+public:
+    Client();
+    Client(const std::string &user, const std::string &nick);
+    Client(const Client &rhs);
+    ~Client();
+
+    Client  &operator=(const Client &rhs);
+
+private:
+
+    std::string _user;
+    std::string _nick;
+
+};
