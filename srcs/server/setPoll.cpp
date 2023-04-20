d95d43b56de020a70eebab82b80b5edbc07ae05d
Théo <74186192+Theoted@users.noreply.github.com>
2023-04-20 11:08:13 +0200

    Everything looks good

--- srcs/server/setPoll.cpp
+++ srcs/server/setPoll.cpp
@@ -43,8 +43,6 @@ int Server::closeConnectionByFd(int fd)
     return (0);
 }
 
-
-
 int Server::handleCtrlD(char *buffer)
 {
     std::string tmp(buffer);
@@ -54,23 +52,12 @@ int Server::handleCtrlD(char *buffer)
         concatenatedCmd += tmp;
         return (1);
     }
-    else if (!detectEOF( buffer) && concatenate)
+    else if (!detectEOF(buffer) && concatenate)
     {
         concatenate = 0;
         concatenatedCmd += tmp;
     }
     return (0);
-}
-
-void printStringInInt(const char *buffer)
-{
-    int i = 0;
-    while (buffer[i])
-    {
-        std::cout << buffer[i] << " ";
-        i++;
-    }
-    std::cout << std::endl << std::endl;
 }
 
 int Server::readExistingConnection(int i)
@@ -81,12 +68,20 @@ int Server::readExistingConnection(int i)
     status = recv(fds[i].fd, buffer, sizeof(buffer), 0);
     if (status < 0)
     {
-        if (errno != EWOULDBLOCK)
+        int error_code;
+        socklen_t error_code_size = sizeof(error_code);
+        if (getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &error_code, &error_code_size) == -1)
         {
-            perror("recv() failed");
+            perror("getsockopt() failed");
             close_conn = TRUE;
+            return (-1);
         }
-        return (-1);
+        if (error_code != EWOULDBLOCK)
+        {
+            std::cerr << "recv() failed with error code" << error_code << std::endl;
+            close_conn = TRUE;
+            return (-1);
+        }
     }
     if (status == 0)
     {
@@ -101,7 +96,6 @@ int Server::readExistingConnection(int i)
             {
                 input = concatenatedCmd;
                 concatenatedCmd = "";
-                printStringInInt(input.c_str());
             }
             if (checkIfNewClient(input.c_str(), fds[i].fd) > 0)
             {
@@ -129,37 +123,29 @@ int Server::acceptIncomingConnection()
     memset(&sin_size, 0, sizeof(socklen_t));
 
     // Loop until a new client connection is accepted
-    do
+
+    // Accept a new client connection on the listening socket
+    // and get a new socket descriptor for the connection
+    new_sd = accept(listen_sd, reinterpret_cast<struct sockaddr *>(&client_addr), &sin_size);
+    // If the accept() call failed
+    if (new_sd < 0)
     {
-        // Accept a new client connection on the listening socket
-        // and get a new socket descriptor for the connection
-        new_sd = accept(listen_sd, reinterpret_cast<struct sockaddr *>(&client_addr), &sin_size);
-        // If the accept() call failed
-        if (new_sd < 0)
-        {
-            // If the error is not EWOULDBLOCK (meaning there are no more connections to accept)
-            if (errno != EWOULDBLOCK)
-            {
-                perror("  accept() failed");
-                // Set the end_server flag to TRUE
-                end_server = TRUE;
-            }
-            // Return -1 to indicate an error occurred
-            return (-1);
-        }
-        // Get the local address and port of the new socket descriptor
-        getsockname(new_sd, reinterpret_cast<struct sockaddr *>(&client_addr), &sin_size);
+        perror("  accept() failed");
+        // Set the end_server flag to TRUE
+        end_server = TRUE;
+        // Return -1 to indicate an error occurred
+        return (-1);
+    }
+    // Get the local address and port of the new socket descriptor
+    getsockname(new_sd, reinterpret_cast<struct sockaddr *>(&client_addr), &sin_size);
 
-        // Create a new client object and add it to the _clientsTryingToConnect map
-        std::cout << "allocating memory for _clientsTryingToConnect\n";
-        this->_clientsTryingToConnect[new_sd] = new Client(new_sd, inet_ntoa(client_addr.sin_addr));
+    // Create a new client object and add it to the _clientsTryingToConnect map
+    this->_clientsTryingToConnect[new_sd] = new Client(new_sd, inet_ntoa(client_addr.sin_addr));
 
-        // Add a new pollfd structure to the fds vector for the new socket descriptor
-        fds.push_back(createPollFdNode(new_sd, POLLIN | POLLHUP));
+    // Add a new pollfd structure to the fds vector for the new socket descriptor
+    fds.push_back(createPollFdNode(new_sd, POLLIN | POLLHUP));
 
-        std::cout << "Accepted connection - " << new_sd << std::endl;
-
-    } while (new_sd != -1);
+    std::cout << "Accepted connection - " << new_sd << std::endl;
 
     return (0);
 }
@@ -203,7 +189,7 @@ int Server::setPoll()
         current_size = fds.size();
         for (int i = 0; i < current_size; i++)
         {
-            //! if no event 
+            //! if no event
             if (fds[i].revents == 0)
                 continue;
             //! if the file descriptor has hang up
@@ -213,7 +199,7 @@ int Server::setPoll()
                 current_size--;
                 continue;
             }
-            //! at this point if fd event different than POLLIN, we sent error 
+            //! at this point if fd event different than POLLIN, we sent error
             if (fds[i].revents != POLLIN)
             {
                 end_server = TRUE;
@@ -233,7 +219,7 @@ int Server::setPoll()
             if (close_conn)
             {
                 closeConnection(i);
-                break ;
+                break;
             }
         }
         std::cout << std::endl;
